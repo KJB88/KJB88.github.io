283738a78817ab838b868303b688a3dda33d6c20
Kevin <KJB88@users.noreply.github.com>
2024-03-04 18:55:12 +0000

    Update common.js

--- assets/js/common.js
+++ assets/js/common.js
@@ -18,9 +18,8 @@ $(document).ready(function () {
     });
     var navSelector = "#toc-sidebar";
     var $myNav = $(navSelector);
-    Toc.init({
-      $nav: $myNav,
-      $scope: $("h1, h2, h3, h4, h5, h6"),
+    Toc.init({$myNav
+      //$scope: $("h1, h2, h3, h4, h5, h6"),
     });
     $("body").scrollspy({
       target: navSelector,
